3c8a3022959c59aefcbd634043a132d9f5bd52f1
Mahdi Moukaddem <mahdi.70.023@gmail.com>
2024-12-10 10:17:13 +0200

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -55,6 +55,30 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+
+        'bucket' => [
+            'driver' => 's3',            
+            'key' => env('R2_SPACES_KEY'),            
+            'secret' => env('R2_SPACES_SECRET'),            
+            'endpoint' => env('R2_SPACES_ENDPOINT'),            
+            'region' => env('R2_SPACES_REGION'),            
+            'bucket' => env('R2_SPACES_BUCKET'),            
+            'root' => rtrim(str_replace(['http://', 'https://'], '', env('APP_URL')), '/'),            
+            'visibility' => 'public',            
+            'throw' => false
+        ],
+        
+        'bucket_private' => [
+            'driver' => 's3',            
+            'key' => env('R2_SPACES_KEY'),            
+            'secret' => env('R2_SPACES_SECRET'),            
+            'endpoint' => env('R2_SPACES_ENDPOINT'),            
+            'region' => env('R2_SPACES_REGION'),            
+            'bucket' => env('R2_SPACES_BUCKET'),            
+            'root' => rtrim(str_replace(['http://', 'https://'], '', env('APP_URL')), '/'),            
+            'visibility' => 'private',            
+            'throw' => false
+        ]
 
     ],
 
