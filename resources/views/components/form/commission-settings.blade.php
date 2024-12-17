19050955c1205748ec6cb8fb52634a4da771e4a8
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2024-12-18 00:13:18 +0200

    upadte

--- resources/views/components/form/commission-settings.blade.php
+++ resources/views/components/form/commission-settings.blade.php
@@ -1,12 +1,4 @@
-<div x-data="{
-    open:false, 
-    conditions : [] , 
-    defaultPercentage : 10, 
-    delete(index){
-        
-    }
-
-}">
+<div x-data="Functions.initConditions()">
     <label class="twa-form-label">
         {{ $info['label'] }}
     </label>
@@ -14,8 +6,7 @@
         <div class="twa-form-input-ring  @isset($info['prefix']) has-prefix @endisset">
 
             @isset($info['prefix'])
-                <span
-                    class="placeholder-class ml-2 mr-1  flex items-center ">{{ $info['prefix'] }}</span>
+                <span class="placeholder-class ml-2 mr-1  flex items-center ">{{ $info['prefix'] }}</span>
             @endisset
             <input x-model="defaultPercentage" type="number" class="twa-form-input ">
         </div>
@@ -26,31 +17,31 @@
         <div class="underline text-xs cursor-pointer py-2" x-on:click="open = !open">
             Conditions
         </div>
-        <div x-show="open" >
-     
-            <template x-for="(condition,index) in conditions">
-                <div class="flex gap-3 items-center mb-2"  wire:key="index">
+        <div x-show="open">
 
-                    
-            
-                        <div class="twa-form-label" style="min-width:50px" >
-                            Week <span x-text="index + 1"></span>
-                        </div>
-                        <div class="twa-form-input-container twa-form-input-ring flex-1">
-                            
-                            <input class="twa-form-input " type="text" x-model="condition">
-                        </div>
-    
-          
-                   
-                    <div x-on:click="delete(index)">
+            <template x-for="(condition,index) in conditions">
+                <div class="flex gap-3 items-center mb-2" wire:key="index">
+
+
+
+                    <div class="twa-form-label" style="min-width:50px">
+                        Week <span x-text="index + 1"></span>
+                    </div>
+                    <div class="twa-form-input-container twa-form-input-ring flex-1">
+
+                        <input class="twa-form-input " type="text" x-model="condition">
+                    </div>
+
+
+
+                    <div x-on:click="deleteCondition(index)">
                         Delete
                     </div>
 
-                       
+
                 </div>
-            </template>   
-            
+            </template>
+
             <button class="btn btn-primary mt-5" type="button" x-on:click="conditions.push('')"> Add Week </button>
 
         </div>
