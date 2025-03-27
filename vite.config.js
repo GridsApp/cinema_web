078f8724e544938f5a9c00c07562b5acd4acfde3
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-03-27 17:22:47 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js', "vendor/twa/uikit/dist/app-BWi0WNwC.js", "vendor/twa/uikit/dist/app-Cn2n5Ab7.css"],
+            input: ['resources/scss/app.scss', 'resources/js/app.js', "vendor/twa/uikit/dist/app-BCxwE___.js", "vendor/twa/uikit/dist/app-CHv-Xk-5.css"],
             refresh: true})]});
