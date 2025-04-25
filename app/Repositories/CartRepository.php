b2ec8731338a30d85674d7e7e17f45ce0091df8d
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-04-25 10:13:21 +0300

    update

--- app/Repositories/CartRepository.php
+++ app/Repositories/CartRepository.php
@@ -90,7 +90,7 @@ class CartRepository implements CartRepositoryInterface
             $cart = new Cart();
             $cart->{$field} = $user_id;
             $cart->system_id = $system_id;
-            $cart->expires_at = now()->addMinutes(5);
+            $cart->expires_at = now()->addMinutes(1);
             $cart->save();
         } catch (Exception $e) {
             throw new Exception($e->getMessage());
