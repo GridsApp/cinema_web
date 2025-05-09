6b5661ade856fd1018c7c3bdaa2097b308ecf45e
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-05-09 15:25:28 +0300

    update

--- config/reports.php
+++ config/reports.php
@@ -4,6 +4,29 @@ return [
 
     'directories' => [
         'app/Reports'
+    ],
+    'mappings' => [
+        "concession-sales-report" => "\App\Reports\ConcessionSalesReport",
+        "coupon-discounts-report" => "\App\Reports\CouponDiscountsReport",
+        "daily-admits-by-item-report" => "\App\Reports\DailyAdmitsByItemReport",
+        "daily-admits-by-type-report" => "\App\Reports\DailyAdmitsByTypeReport",
+        "daily-admits-report" => "\App\Reports\DailyAdmitsReport",
+        "daily-admits-report-by-distributor" => "\App\Reports\DailyAdmitsReportByDistributor",
+        "deferred-income-report" => "\App\Reports\DeferredIncomeReport",
+        "erp-cafeteria-report" => "\App\Reports\ErpCafeteriaReport",
+        "erp-integration-report" => "\App\Reports\ErpIntegrationReport",
+        "imtiyaz-report" => "\App\Reports\ImtiyazReport",
+        "loyalty-transactions-report" => "\App\Reports\LoyaltyTransactionsReport",
+        "order-cafeteria-report" => "\App\Reports\OrderCafeteriaReport",
+        "order-glasses-report" => "\App\Reports\OrderGlassesReport",
+        "order-tickets-report" => "\App\Reports\OrderTicketsReport",
+        "order-topups-report" => "\App\Reports\OrderTopupsReport",
+        "price-card-summary-report" => "\App\Reports\PriceCardSummaryReport",
+        "soa-report" => "\App\Reports\SoaReport",
+        "tickets-report" => "\App\Reports\TicketsReport",
+        "wallet-cards-report" => "\App\Reports\WalletCardsReport",
+        "wallet-topups-report" => "\App\Reports\WalletTopupsReport",
+        "wallet-transactions-report" => "\App\Reports\WalletTransactionsReport"
     ]
 
 ];
