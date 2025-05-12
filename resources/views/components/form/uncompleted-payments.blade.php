6703936bda373a373312de2f1c068e098d1eff40
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-05-12 09:45:46 +0300

    update

--- resources/views/components/form/uncompleted-payments.blade.php
+++ resources/views/components/form/uncompleted-payments.blade.php
@@ -1,9 +1,9 @@
 <div>
- 
 
-    @component('CMSView::components.panels.default', ['classes' => 'manage-wallet-panel', 'title' => 'Uncompleted Payments' 
-            
-        
+
+    @component('CMSView::components.panels.default', [
+        'classes' => 'manage-wallet-panel',
+        'title' => 'Uncompleted Payments',
     ])
         <table class="twa-table table-auto">
             <thead>
@@ -16,14 +16,14 @@
                     <th> Message </th>
 
                     <th> Action </th>
-                   
+
                 </tr>
 
             </thead>
             <tbody>
 
                 @forelse($rows as $row)
-                {{-- @dd($row); --}}
+                    {{-- @dd($row); --}}
                     <tr>
                         <td> {{ $row->barcode }} </td>
                         <td> {{ $row->user_id }} </td>
@@ -31,28 +31,29 @@
                         <td> {{ $row->label }} </td>
                         <td> {{ $row->payment_reference }} </td>
                         <td> {{ $row->message }} </td>
-                        <td> 
+                        <td>
 
+{{-- 
+                            <button type="button" class="btn btn-primary" wire:click="treatPayment('{{ $row->id }}')">
+                                Treat</button> --}}
 
-                            <button type="button" class="btn btn-primary" wire:click="treatPayment('{{$row->id}}')" > Treat</button>
+                            <div x-data="{ showModal: false, handleOpen() { this.showModal = true } }">
+                                <button type="button" class="btn btn-primary" @click="handleOpen"> Treat</button>
 
-                            {{-- <div x-data="{ showModal: false, handleOpen() { this.showModal = true } }">
-                            <button type="button" class="btn btn-primary" @click="handleOpen" > Treat</button>
-                        
-                            @component('UIKitView::components.modal', [
-                                'title' => 'Treat',
-                                'variable' => 'showModal',
-                                'action' => [
-                                    'label' => "'Treat'",
-                                    'type' => 'success',
-                                    'handler' => "treatPayment('".$row->id."')",
-                                ],
-                            ])
-                                <div class="text-[13px] font-medium text-left text-gray-800 p-5">
-                                    Are you sure you want to treat this payment?
-                                </div>
-                            @endcomponent
-                            </div> --}}
+                                @component('UIKitView::components.modal', [
+                                    'title' => 'Treat',
+                                    'variable' => 'showModal',
+                                    'action' => [
+                                        'label' => "'Treat'",
+                                        'type' => 'success',
+                                        'handler' => "treatPayment('" . $row->id . "')",
+                                    ],
+                                ])
+                                    <div class="text-[13px] font-medium text-left text-gray-800 p-5">
+                                        Are you sure you want to treat this payment?
+                                    </div>
+                                @endcomponent
+                            </div>
 
                         </td>
                     </tr>
@@ -63,11 +64,11 @@
 
                     </tr>
                 @endforelse
-               
+
             </tbody>
         </table>
     @endcomponent
 
-    
+
 
 </div>
