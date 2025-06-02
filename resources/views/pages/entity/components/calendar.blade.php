4be44e4f394f051f7f1096e3465e64670ca258a4
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-06-02 11:36:41 +0300

    update

--- resources/views/pages/entity/components/calendar.blade.php
+++ resources/views/pages/entity/components/calendar.blade.php
@@ -65,10 +65,10 @@
                                 @if(cms_check_permission('delete-movie-shows'))
                                 <template x-if="selected.length > 0">
 
-                                    <button x-on:click="deleteMovieShows()" type="button"
+                                    {{-- <button x-on:click="deleteMovieShows()" type="button"
                                         class="text-[12px] focus:ring-offset-white focus:shadow-outline group inline-flex items-center justify-center gap-x-2 border outline-none transition-all duration-200 ease-in-out hover:shadow-sm focus:border-transparent focus:ring-2 disabled:cursor-not-allowed disabled:opacity-80  px-4 py-2 text-red-50 ring-red-500 bg-red-500 focus:bg-red-600 hover:bg-red-600 border-transparent focus:ring-offset-2 dark:focus:ring-offset-dark-900 dark:focus:ring-red-600 dark:bg-red-700 dark:hover:bg-red-600 dark:hover:ring-red-600 rounded-md">
                                         Delete
-                                    </button>
+                                    </button> --}}
 
                                 </template>
                                @endif
