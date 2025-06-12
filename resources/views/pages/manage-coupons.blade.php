330ce898b2450ff4e78066c1766a6a72bc0f83c9
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-06-12 10:48:23 +0300

    update

--- resources/views/pages/manage-coupons.blade.php
+++ resources/views/pages/manage-coupons.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+<style>
+    .twa-table tbody tr td:last-child{
+        position: unset !important;
+    }
+    .twa-table thead th:last-child{
+        position: unset !important;
+    }
+</style>
 <div class="container-fixed">
   
     <livewire:components.manage-coupons />
