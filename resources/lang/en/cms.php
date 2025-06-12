b3faef5d8936281dcefed5070622403946befb02
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-06-12 11:46:42 +0300

    update

---
+++ resources/lang/en/cms.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'text' => 'Cinema Access Portal',
+    'description' => 'Your centralized hub for managing movies, showtimes, and theaters
+    to help you streamline operations and deliver a seamless cinematic experience to your audience',
+  
+];
