257cf2319c86134163a9933e741a669b357915a3
hovigsenekjian <hovig@thewebaddicts.com>
2025-08-05 09:47:19 +0300

    updates

---
+++ config/omnipush.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'onesignal' => [
+        "provider" => "onesignal",
+        "data" => [ "AppID" => "47689795-2ec8-4a15-a738-5345453ed8b9", "AuthKey" => "MGU4YzIwNDktZTUxYi00NmJlLThhNjctYzQ0MWIxNzhjYzFj" ],
+    ]
+];
+
