db6a0d798f13b4196744dfb4650ee78f11a2399d
WebGrids <gridsapp1@gmail.com>
2025-08-21 08:51:12 +0300

    Add files via upload

---
+++ config/settings.php
@@ -0,0 +1,166 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'field' => 'facebook_label',
+    'group' => 'Social Media',
+    'translatable' => true,
+    'value_ar' => 'Iraqi Cinema',
+    'value_en' => 'Iraqi Cinema',
+    'value_fr' => 'Iraqi Cinema fr',
+  ),
+  1 => 
+  array (
+    'field' => 'facebook',
+    'group' => 'Social Media',
+    'value' => 'https://www.facebook.com/share/Ws8StHgQcg4ccGFX',
+    'translatable' => false,
+  ),
+  2 => 
+  array (
+    'field' => 'whattsapp_label',
+    'group' => 'Social Media',
+    'translatable' => true,
+    'value_ar' => 'تواصل معنا ',
+    'value_en' => 'Call us',
+    'value_fr' => NULL,
+  ),
+  3 => 
+  array (
+    'field' => 'whattsapp',
+    'group' => 'Social Media',
+    'value' => 'https://wa.me/qr/C5K5C63ZFWJXC1',
+    'translatable' => false,
+  ),
+  4 => 
+  array (
+    'field' => 'instagram_label',
+    'group' => 'Social Media',
+    'translatable' => true,
+    'value_ar' => 'Iraqi Cinema',
+    'value_en' => 'Iraqi Cinema',
+    'value_fr' => NULL,
+  ),
+  5 => 
+  array (
+    'field' => 'instagram',
+    'group' => 'Social Media',
+    'value' => 'https://www.instagram.com/iraqicinema/?igshid=MXdsZmludXV5dWI4cg==',
+    'translatable' => false,
+  ),
+  6 => 
+  array (
+    'field' => 'x_label',
+    'group' => 'Social Media',
+    'translatable' => true,
+    'value_ar' => NULL,
+    'value_en' => '',
+    'value_fr' => NULL,
+  ),
+  7 => 
+  array (
+    'field' => 'x',
+    'group' => 'Social Media',
+    'value' => '',
+    'translatable' => false,
+  ),
+  8 => 
+  array (
+    'field' => 'minimum_topup_amount',
+    'group' => 'Validation Topup Amount',
+    'value' => '1000',
+    'translatable' => false,
+  ),
+  9 => 
+  array (
+    'field' => 'maximum_topup_amount',
+    'group' => 'Validation Topup Amount',
+    'value' => '200000',
+    'translatable' => false,
+  ),
+  10 => 
+  array (
+    'field' => 'financial_phone',
+    'group' => 'Contact Settings',
+    'value' => '7006',
+    'translatable' => false,
+  ),
+  11 => 
+  array (
+    'field' => 'financial_email',
+    'group' => 'Contact Settings',
+    'value' => 'info@iraqicinema.net',
+    'translatable' => false,
+  ),
+  12 => 
+  array (
+    'field' => 'operator_phone',
+    'group' => 'Contact Settings',
+    'value' => '',
+    'translatable' => false,
+  ),
+  13 => 
+  array (
+    'field' => 'operator_email',
+    'group' => 'Contact Settings',
+    'value' => 'job@iraqicinema.net',
+    'translatable' => false,
+  ),
+  14 => 
+  array (
+    'field' => 'management_phone',
+    'group' => 'Contact Settings',
+    'value' => '',
+    'translatable' => false,
+  ),
+  15 => 
+  array (
+    'field' => 'management_email',
+    'group' => 'Contact Settings',
+    'value' => 'management@email.com',
+    'translatable' => false,
+  ),
+  16 => 
+  array (
+    'field' => 'currency',
+    'group' => 'Currency',
+    'value' => 'IQD',
+    'translatable' => false,
+  ),
+  17 => 
+  array (
+    'field' => 'time_period',
+    'group' => 'Period',
+    'value' => '14:00',
+    'translatable' => false,
+  ),
+  18 => 
+  array (
+    'field' => 'timer_expire_cart_pos',
+    'group' => 'Timer Expire Cart',
+    'value' => '60',
+    'translatable' => false,
+  ),
+  19 => 
+  array (
+    'field' => 'timer_expire_cart_app',
+    'group' => 'Timer Expire Cart',
+    'value' => '5',
+    'translatable' => false,
+  ),
+  20 => 
+  array (
+    'field' => 'timer_expire_cart_kiosk',
+    'group' => 'Timer Expire Cart',
+    'value' => '10',
+    'translatable' => false,
+  ),
+  21 => 
+  array (
+    'field' => 'show_remove_time_offset',
+    'group' => 'Show Remove Time offset (minutes)',
+    'value' => '45',
+    'translatable' => false,
+  ),
+);
